5ae55bb666e96c18ba01001841251e6e4a177bda
Ana <aninha10.desouza@gmail.com>
2024-03-07 17:30:39 -0300

    Criando pastas

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,42 +1,16 @@
 import logo from "./assets/logo.png";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from './components/pages/Home'
-import Category from './components/pages/Category'
-import Product from './components/pages/Product'
-import History from './components/pages/History'
-import Detail from './components/pages/Detail'
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Home from './components/pages/Home'
+// import Category from './components/pages/Category'
+// import Product from './components/pages/Product'
+// import History from './components/pages/History'
+// import Detail from './components/pages/Detail'
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Router>
-        <div className="App">
-          <ul>
-            <li>Home</li>
-            <li>Contato</li>
-          </ul>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/">
-              <Category />
-            </Route>
-            <Route exact path="/">
-              <Product />
-            </Route>
-            <Route exact path="/">
-              <History />
-            </Route>
-            <Route exact path="/">
-              <Detail />
-            </Route>
-          </Switch>
-
-          <img className="logo" src={logo}></img>
-        </div>
-      </Router>
+     <img src={logo}  />
     </>
   );
 }
